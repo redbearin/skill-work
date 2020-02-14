22d7287316b22f81b3b3161d71e5495af7514f52
Juliann <perrigoj@bellsouth.net>
2020-02-14 23:33:47 +0000

    calculate max product using three numbers in an array

---
+++ max-product-3-numbers/answer.js
@@ -0,0 +1,12 @@
+function maximumProduct(nums) {
+  nums = nums.sort((a, b) => a - b);
+  var product = 1;
+  for (var index = nums.length - 1; index > nums.length - 4 ; index--) {
+    product *= nums[index];
+  }
+  return product;
+};
+
+var nums = [10,9,4,5,3,2];
+
+maximumProduct(nums);
