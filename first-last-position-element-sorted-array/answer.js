1fa2bd511010f20ff761898de7f7f3274fce66fd
Juliann Talkington <perrigoj@bellsouth.net>
2020-03-30 22:11:12 -0700

    first and last positions of a target number in an array -- O(log n)

---
+++ first-last-position-element-sorted-array/answer.js
@@ -0,0 +1,23 @@
+function searchRange(nums, target) {
+  let lowIndex = 0;
+  let highIndex = nums.length  - 1;
+  while(lowIndex < highIndex) {
+      const midIndex = Math.floor((lowIndex + highIndex) / 2);
+      nums[midIndex] >= target ? highIndex = midIndex : lowIndex = midIndex + 1;
+  }
+  if (nums[lowIndex] !== target) {
+      return [-1,-1];
+  }
+  const actualLowIndex = lowIndex; 
+  highIndex = nums.length - 1;
+  while (lowIndex < highIndex) { 
+      const midIndex = Math.floor((lowIndex + highIndex) / 2);
+      nums[midIndex] <= target ? lowIndex = midIndex + 1 : highIndex = midIndex;
+  }
+  return nums[highIndex] === target ? [actualLowIndex, highIndex] : [actualLowIndex, highIndex - 1];
+};
+
+const nums = [2,3,3,4,5,5,6,7,8];
+const target = 3;
+
+searchRange(nums, target);
