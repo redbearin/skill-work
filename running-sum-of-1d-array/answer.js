48a6c221c6bb1d6fa00b0b51f1e4909c566f2bf6
Juliann Talkington <perrigoj@bellsouth.net>
2020-07-05 12:02:37 -0700

    add comments to forEach solution

--- running-sum-of-1d-array/answer.js
+++ running-sum-of-1d-array/answer.js
@@ -1,6 +1,8 @@
 const runningSum = (nums) => {
   let sum = 0;
   let sumArray = [];
+  // cycles through each number using forEach 
+  // and increase sum; push latest sum to sumArray
   nums.forEach(number => {
     sum += number;
     sumArray.push(sum);
