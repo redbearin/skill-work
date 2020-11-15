7d94b3bed0ef427f192badf26d8fd54b57a7aa72
Juliann Talkington <perrigoj@bellsouth.net>
2020-11-15 08:54:26 -0700

    count number of additions of digits to get to a one digit number

---
+++ persistence/answer.js
@@ -0,0 +1,23 @@
+const additivePersistence = (num) => {
+  // convert number to a string
+  let strNum = num.toString();
+  let newNum = 0;
+  let count = 0;
+  while (strNum.length > 1) {
+    // cycle through each digit of number
+    for(let index = 0; index < strNum.length; index++) {
+      newNum += +strNum[index];
+    }
+    // set value of newNum to strNum
+    strNum = newNum.toString();
+    // increment the count
+    count++;
+    // reset newNum
+    newNum = 0;
+  }
+  return count;
+};
+
+const num = 6;
+
+additivePersistence(num);
