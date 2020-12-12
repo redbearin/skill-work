a625ecfc3c1b8057f502bb35998c1c8dc57fcf53
Juliann Talkington <perrigoj@bellsouth.net>
2020-12-11 20:55:30 -0700

    add two string numbers CORRECTED

--- add-two-string-numbers/answer.js
+++ add-two-string-numbers/answer.js
@@ -4,9 +4,9 @@ const sum2 = (num1, num2) => {
     return "-1";
   // replace '' with zero
   if (num1 === '')
-    num1 = 0;
+    num1 = '0';
   if (num2 === '')
-    num2 = 0;  
+    num2 = '0';  
   // amount to carry between columns
   let carry = 0;
   // string of sum
