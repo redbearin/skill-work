4f20727aa8f4f69d48d61500c8625c94320182c0
Juliann Talkington <perrigoj@bellsouth.net>
2021-06-19 18:45:42 -0700

    find all the letters that match between two words

---
+++ find-the-shared-letters-between-two-strings/README.md
@@ -0,0 +1,14 @@
+Given two strings, return a string containing only the letters shared between the two.
+
+Examples
+sharedLetters("house", "home") ➞ "eho"
+
+sharedLetters("Micky", "mouse") ➞ "m"
+
+sharedLetters("house", "villa") ➞ ""
+
+Notes sharedLetters:
+If none of the letters are shared, return an empty string.
+The function should be case insensitive (e.g. comparing A and a should return a).
+Sort the resulting string alphabetically before returning it.
+handled more than one match of a letter
---
+++ find-the-shared-letters-between-two-strings/answer.js
@@ -0,0 +1,28 @@
+const sharedLetters = (word1, word2) => {
+  const word2Arr = word2.split('');
+  let lowerLtr, index;
+  const matches = [];
+  // go letter by letter through first word
+  // check to see if second word includes
+  // that letter 
+  for (let i = 0; i < word1.length; i++) {
+    lowerLtr = word1[i].toLowerCase();
+    // index of match in second word
+    index = word2Arr.indexOf(lowerLtr);
+    // if letter in both words
+    // mark letter as matchedd
+    // add letter to matches
+    if (index >= 0) {
+      word2Arr[index] = '*';
+      matches.push(lowerLtr);
+    }
+
+  }
+  // sort the letters in alpha order
+  // then join into a string
+  return matches.sort().join('');
+}
+const word1 = "hello";
+const word2 = "yellow";
+
+document.getElementById('ans').textContent = sharedLetters(word1, word2);
